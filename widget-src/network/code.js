a6744f1dfd9f2e09d1fbbe466b801aef92e73808
jaeryo2357 <jaeryo2357@naver.com>
2024-11-02 18:35:45 +0900

    notion api 요청하는 코드 추가하기

---
+++ widget-src/network/code.js
@@ -0,0 +1,30 @@
+async function fetchNotionData(apiKey, blockUrl) {
+    const notionPageId = getBlockIdAndNotify(blockUrl); // URL에서 ID 추출하는 함수
+    if (notionPageId == null) return
+    const response = await fetch(`https://api.notion.com/v1/blocks/${notionPageId}`, {
+      headers: {
+        "Authorization": `Bearer ${apiKey}`,
+        "Notion-Version": "2022-06-28"
+      }
+    });
+    const data = await response.json();
+    return data;
+  }
+
+function extractBlockIdFromUrl(url) {
+    // 32자리의 영숫자 ID를 찾기 위한 정규식
+    const match = url.match(/[0-9a-fA-F]{32}/);
+    if (match) {
+      return match[0];
+    }
+    throw new Error("유효한 ID를 URL에서 찾을 수 없습니다.");
+}
+
+function getBlockIdAndNotify(url) {
+    try {
+      return extractBlockIdFromUrl(url);
+    } catch (error) {
+      figma.notify("URL에서 tion 페이지No ID를 찾을 수 없습니다. URL을 확인해주세요.");
+      return null;
+    }
+  }
